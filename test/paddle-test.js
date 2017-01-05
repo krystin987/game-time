ce5b0db6a5de3032546b55cdc8ffb320f9360422
Krystin Villeneuve <krystinv@gmail.com>
2017-01-05 16:54:15 -0700

    Added property tests to paddle

--- test/paddle-test.js
+++ test/paddle-test.js
@@ -1,6 +1,12 @@
 var chai = require('chai');
 var assert = chai.assert;
 var Paddle = require('../lib/paddle');
+var paddle = new Paddle();
+
+// expect some of this to change when we decide/commit to
+// board/piece sizing, use any values for now - or
+// maybe estimate them, the board dimensions
+// may be more subjective than tetris?
 
 describe ('Paddle', function(){
   context('default', function(){
@@ -9,6 +15,37 @@ describe ('Paddle', function(){
       var paddle = new Paddle();
       assert.isFunction(Paddle);
     });
+
+    it('should have a canvas property', function(){
+      assert.property(paddle, 'canvas')
+    });
+
+    it('should have a context property', function(){
+      assert.property(paddle, 'context');
+    });
+
+// the 1 setting here is for test purposes ONLY
+    it.skip('should have a height property set to 1', function(){
+      assert.property(paddle, '1');
+    });
+
+// the 1 setting here is for test purposes ONLY
+    it.skip('should have a width property set to 1', function(){
+      assert.property(paddle, '1');
+    });
+
+// the 1 setting here is for test purposes ONLY and must be updated
+    it.skip('should have an x property set to equal half of the width of the canvas minus the value of the width property', function(){
+      assert.property(paddle.x, '1');
+    });
+
+// the 1 setting here is for test purposes ONLY and must be updated
+    it.skip('should have a y property set to equal the height of the canvas property minus the value of the height property', function(){
+      assert.property(paddle.y, '1');
+    });
+
+    it.skip('')
+
 
 
 
