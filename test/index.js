dd81fd820a9a1a45ef6c0cc0055c17519730471a
Krystin Villeneuve <krystinv@gmail.com>
2017-01-05 21:55:00 -0700

    Implement moving paddle object animation and paddle tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 // require('./ball-test.js');
-// require('./paddle-test.js');
+require('./paddle-test.js');
 // require('./blockwall-test.js');
 // // require('./scoreboard-test.js');
-// // require('./gametime-test.js');
+require('./breakout-game-test.js');
--- test/paddle-test.js
+++ test/paddle-test.js
@@ -1,45 +1,58 @@
 var chai = require('chai');
 var assert = chai.assert;
 var Paddle = require('../lib/paddle');
-
-// expect some of this to change when we decide/commit to
-// board/piece sizing, use any values for now - or
-// maybe estimate them, the board dimensions
-// may be more subjective than tetris?
+// var canvas = canvas;
+// var context = context;
 
 describe ('Paddle', function(){
   context('default', function(){
+      var paddle = new Paddle(5, 10, 10, 10);
 
-    it('Paddle should be a function', function(){
-      var paddle = new Paddle();
+    // The paddle should be a function
+    it('Paddle should be a function', function() {
+      // var paddle = new Paddle();
       assert.isFunction(Paddle);
     });
 
+    // The paddle should have a property for context
+    it.skip('should have a context property', function() {
+      // var paddle = new Paddle();
+      assert.property(paddle, 'context');
+    });
 
-// The paddle should have an x coordiante
-    it('paddle should have a value for x', function(){
-      var paddle = new Paddle(1);
-      assert.equal(paddle.x, 1);
+    // The paddle should have a property for canvas
+    it.skip('should have a canvas property', function() {
+      // var paddle = new Paddle();
+      assert.property(paddle, 'canvas');
+    });
+
+    // The paddle should have a width
+    it.skip('Paddle should have a width of 205 by default', function() {
+      // var paddle = new Paddle();
+      assert.equal(paddle.width, 180);
+    });
+    //
+    it.skip('Paddle should have a height of 20 by default', function() {
+      // var paddle = new Paddle();
+      assert.equal(paddle.height, 20);
+    });
+
+    // The paddle should have an x coordinate
+    it.skip('paddle should have a value for x', function() {
+      // var paddle = new Paddle(x);
+      assert.equal(paddle.x, 5);
     });
 
 // The paddle should have a y coordinate
-    it('Paddle should have a vale for y', function(){
-      var paddle = new Paddle(1, 2);
-      assert.equal(paddle.y, 2);
+    it.skip('Paddle should have a value for y', function() {
+      // var paddle = new Paddle(1, 2);
+      assert.equal(paddle.y, 10);
     });
 
-// The paddle should have a width
-    it('Paddle should have a width', function(){
-      var paddle = new Paddle(1,3,4,5,);
-      assert.equal(paddle.width, 4);
+    it.skip('should increment x by 1 when the right arrow is pressed', function() {
+      // var paddle = new Paddle(2, 2, 2, 2);
+      assert.equal(paddle.x, 3);
     });
-
-// the paddle should have a height
-    it('Paddle should have a height', function(){
-      var paddle = new Paddle(1,3,4,5);
-      assert.equal(paddle.height, 5);
-    });
-
 
   });
 });
