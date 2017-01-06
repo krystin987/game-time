143d04592bc6e89e6c1b947bfa300959b11108e7
Rebecca Holt <rebecca.holt9991@gmail.com>
2017-01-06 08:30:54 -0700

    resolving conflicts

--- test/index.js
+++ test/index.js
@@ -1,9 +1,5 @@
 // require('./ball-test.js');
-<<<<<<< HEAD
-require('./paddle-test.js');
-=======
 require('./paddle-test');
->>>>>>> 4fb64d22627ce5922459a6e8e2a4318968ea9e13
 // require('./blockwall-test.js');
 // // require('./scoreboard-test.js');
 require('./breakout-game-test');
