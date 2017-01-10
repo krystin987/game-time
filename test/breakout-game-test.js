a51a6798ef9d4b5ab19e7e9c6ad54a38f58cdc8e
Krystin Villeneuve <krystinv@gmail.com>
2017-01-10 16:50:45 -0700

    Add hilarious music file

--- test/breakout-game-test.js
+++ test/breakout-game-test.js
@@ -28,8 +28,8 @@ describe('Game', function(){
       assert.property(game, 'ball');
     });
 
-    it('should have a default gameState of 1', function () {
-      assert.equal(game.gameState, 1);
+    it('should have a default gameState of 0', function () {
+      assert.equal(game.gameState, 0);
     });
 
     context('on gameState functions', function(){
@@ -48,9 +48,9 @@ describe('Game', function(){
         assert.isFunction(game.gameOver);
       });
 
-      it('should have a function for gamePause', function() {
-        assert.isFunction(game.gamePause);
-      });
+      // it('should have a function for gamePause', function() {
+      //   assert.isFunction(game.gamePause);
+      // });
 
       it('should have a function for animate', function() {
         assert.isFunction(game.animate);
@@ -59,6 +59,41 @@ describe('Game', function(){
       it('should have a function for timing', function(){
         assert.isFunction(game.timing);
       });
+
+    });
+
+    context('on collision functions', function(){
+      var game = new Game({
+        x: this.x,
+        y: this.y,
+        context: {},
+        canvas: {}
+      });
+
+      it('should have a function to test collision with the sides of the canvas and the ball', function(){
+        assert.isFunction(game.collisionLeftRight);
+      });
+
+      it('should have a function to test collision with the bottom of the canvas and the ball', function(){
+        assert.isFunction(game.collisionBottom);
+      });
+
+      it('should have a function to test collision with the ball and the top of the canvas', function(){
+        assert.isFunction(game.collisionTop);
+      });
+
+      it('should have a function to test for collision with the ball and the paddle', function(){
+        assert.isFunction(game.collisionBallPaddle);
+      });
+
+      it('should have a function to change ball direction upon collision with the sides of the canvas or paddle', function(){
+        assert.isFunction(game.wallPaddleCollision);
+      });
+
+      // it('when the ball collides with any canvas side or the game paddle, the ball should change direction', function(){
+      //
+      // });
+
 
     });
 
